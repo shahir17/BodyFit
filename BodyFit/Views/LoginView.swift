7ad37ce4e56eb91d2cfadacc4bab9387dbc4d4ad
Ahmad Shahir Abdul Satar <abdulsataas2@vcu.edu>
2021-07-23 22:17:32 -0400

    initial commit

---
+++ BodyFit/Views/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  BodyFit
+//
+//  Created by Ahmad Shahir Abdul Satar on 7/23/21.
+//
+
+import Foundation
---
+++ BodyFit/Views/SignupView.swift
@@ -0,0 +1,8 @@
+//
+//  SignupView.swift
+//  BodyFit
+//
+//  Created by Ahmad Shahir Abdul Satar on 7/23/21.
+//
+
+import Foundation
